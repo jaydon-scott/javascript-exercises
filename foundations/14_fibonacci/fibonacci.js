b101ef3610f7234e90b1e2eab4eb1aab1dd4491b
jscott <jaydon.r.scott@gmail.com>
2026-01-04 20:21:53 -0700

    Complete 14_fibonnaci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (fibIndex) {
+  fibIndex = parseInt(fibIndex);
+  if (fibIndex === 0) return 0;
+  if (fibIndex === 1) return 1;
+  if (fibIndex < 0) return "OOPS";
 
+  let fibonnaciList = [1, 1];
+
+  while (fibonnaciList.length != fibIndex) {
+    fibonnaciList.push(fibonnaciList.at(-1) + fibonnaciList.at(-2));
+  }
+  return fibonnaciList.at(-1);
 };
 
 // Do not edit below this line
