a682f0a512765a9571f76baa14b40b52292e2a76
jscott <jaydon.r.scott@gmail.com>
2026-01-04 17:59:44 -0700

    Complete 13_palindromes exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (inputStr) {
+  // For handling uppercase words
+  inputStr = inputStr.toLowerCase();
 
+  // For handling punctuation (e.g. !, .)
+  inputStr = inputStr.replaceAll("!", "");
+  inputStr = inputStr.replaceAll(".", "");
+  inputStr = inputStr.replaceAll(",", "");
+
+  // For handling multiple words
+  inputStr = inputStr.replaceAll(" ", "");
+
+  return inputStr === [...inputStr].reverse().join("");
 };
 
 // Do not edit below this line
