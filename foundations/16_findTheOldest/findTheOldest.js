e92c6b0ec3556e5f23f3ca9fb7cc1fcf307c5b59
jscott <jaydon.r.scott@gmail.com>
2026-01-05 20:46:59 -0700

    Complete 16_findTheOldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const oldestPerson = people.sort((a, b) => {
+    let currentYear = new Date().getFullYear();
+    let yearOfDeath_a = "yearOfDeath" in a ? a.yearOfDeath : currentYear;
+    let yearOfDeath_b = "yearOfDeath" in b ? b.yearOfDeath : currentYear;
+    return yearOfDeath_a - a.yearOfBirth < yearOfDeath_b - b.yearOfBirth ? 1 : -1;
+  })[0];
+  return oldestPerson;
 };
 
 // Do not edit below this line
